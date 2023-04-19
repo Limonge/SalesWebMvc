256314d20445630787a34af849231f0e5c7e639d
Claudio Limonge <claudio.limonge@gmail.com>
2023-04-19 14:58:53 -0300

    Simple Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -15,8 +16,20 @@ namespace SalesWebMvc.Controllers
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
-
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // indica que a ação é post
+        [ValidateAntiForgeryToken] // para evitar ataques CRSF
+        public IActionResult Create (Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); // redireciona para Index 
         }
     }
 }
